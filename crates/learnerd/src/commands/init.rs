f2ea757e9510f91d40eb6b9174a2229059b445e4
Colin Roberts <colin@autoparallel.xyz>
2024-11-26 09:00:24 -0700

    fix: directory creation

--- crates/learnerd/src/commands/init.rs
+++ crates/learnerd/src/commands/init.rs
@@ -67,18 +67,17 @@ pub async fn init<I: UserInteraction>(interaction: &mut I, init_args: InitArgs) 
     config
   };
 
-  std::fs::write(Config::default_path()?.join("config.toml"), toml::to_string(&config)?)?;
-
   // Create learner with this configuration and with the default retrievers (arXiv, DOI, IACR)
   if default_retrievers {
     interaction
       .reply(ResponseContent::Info("Using the default set of retrievers (arXiv and DOI)."))?;
-
+    std::fs::create_dir_all(Config::default_path()?.join("retrievers"))?;
     std::fs::write(config.retrievers_path.join("arxiv.toml"), learner::ARXIV_CONFIG)?;
     std::fs::write(config.retrievers_path.join("doi.toml"), learner::DOI_CONFIG)?;
     std::fs::write(config.retrievers_path.join("iacr.toml"), learner::IACR_CONFIG)?;
   }
   Learner::builder().with_config(config.clone()).build().await?;
+  std::fs::write(Config::default_path()?.join("config.toml"), toml::to_string(&config)?)?;
   interaction.reply(ResponseContent::Success(&format!(
     "Created Learner configuration with\nConfig path: {:?}\nDatabase path: {:?}\nDocument storage \
      path: {:?}",
